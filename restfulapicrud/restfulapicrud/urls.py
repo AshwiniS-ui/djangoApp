ba554a457007e7ccc97724fde09e9e917ad7e55e
Ashwini S <67921353+AshwiniS-ui@users.noreply.github.com>
2021-08-10 17:16:27 +0530

    Update urls.py

--- restfulapicrud/restfulapicrud/urls.py
+++ restfulapicrud/restfulapicrud/urls.py
@@ -16,10 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('studentapi/', include('studentapi.urls')),
-# ]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('studentapi/', include("studentapi.urls")),
